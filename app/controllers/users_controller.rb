3a3a0eb2bc61b8975690a6d2f241885d1d08a0c1
Team_Tornado <prashanthkumar374@gmail.com>
2012-07-16 10:24:11 +0530

    users done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       flash[:notice] = "Account registered!"
+      
       redirect_back_or_default account_url
     else
       render :action => :new
